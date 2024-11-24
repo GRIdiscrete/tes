7179f05d4916e29dadeec652ab07977165f80309
GRIdiscrete <kjaure@gorillaresearch.net>
2024-11-24 11:45:21 +0200

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -17,7 +17,7 @@ const Experience = () => {
           position={[10, 10, 5]}
           intensity={1.5}
         />
-        <Car />
+        <Car scale={[1.1, 1.1, 1.1]}/>
         <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]}> <ringGeometry args={[3, 3.2, 132]} /> <meshBasicMaterial color={0x800080} side={THREE.DoubleSide} /> </mesh>
         <Environment preset="city" />
       </PresentationControls>
